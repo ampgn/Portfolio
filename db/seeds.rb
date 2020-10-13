d69a2baef36221260bf8ecd35972d1a39ed1a508
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2020-10-13 23:31:27 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+portfolio1 = Page.create(title: 'Food-Trotter', description: 'Si pour vous voyager rime avec découverte culinaire, vous allez adorer Food Trotter 🍣 🍕
+La nouvelle app qui vous donnera toutes les infos sur les spécialités locales et les meilleurs restos où les consommer #EatingisVisting')
+portfolio2 = Page.create(title: 'Barbhack', description: 'Barbhack est une conférence, des ateliers, un CTF de hacking !')
