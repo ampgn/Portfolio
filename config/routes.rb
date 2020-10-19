62762e9a13faf24a6a03e95ebcdbf05811d59bd3
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2020-10-19 17:23:31 +0200

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages' => 'pages#welcome' 
-  root 'pages#welcome'
+  get 'welcome' => 'pages#welcome' 
+  get 'home' => 'pages#home'
+  root 'pages#home'
   get 'portfolio' => 'pages#portfolio', as: 'portfolio'
   get 'about' => 'pages#about', as: 'about'
   get 'contact' => 'pages#contact', as: 'contact'
